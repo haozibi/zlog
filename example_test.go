f8dd12b8657ed9f81c28d0733d52da4b6e1c9fd6
haozibi <haozibi@foxmail.com>
2019-06-17 18:56:14 +0800

    test: fix example code

--- example_test.go
+++ example_test.go
@@ -3,7 +3,7 @@ package zlog
 import "os"
 
 func Example() {
-	NewBasicLog(os.Stdout, WithNoColor(true), WithDebug(true))
+	NewBasicLog(os.Stdout, WithNoColor(true), WithDebug(true), WithTimeFormat("2006"))
 	// zlog.NewJSONLog(os.Stdout)
 
 	ZInfo().
@@ -14,6 +14,6 @@ func Example() {
 		Float64("f", 3.1415926).
 		Msgf("hello %s", "zlog")
 	//Output:
-	//2019-06-17 16:10:18.998152 INF example_test.go:11 > just do it z=99
-	//2019-06-17 16:10:18.998741 DBG example_test.go:15 > hello zlog f=3.1415926
+	// 2019 INF example_test.go:11 > just do it z=99
+	// 2019 DBG example_test.go:15 > hello zlog f=3.1415926
 }
